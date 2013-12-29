e175fd3bce7e8b2e310b82cd4a54d584c7409550
Peter Zakin <peter.zakin@gmail.com>
2013-12-29 15:37:07 -0500

    adds PBClient as a way to access core data

---
+++ PhotoBomb/PhotoBomb/PBClient.h
@@ -0,0 +1,20 @@
+//
+//  PBClient.h
+//  PhotoBomb
+//
+//  Created by Peter Zakin on 12/28/13.
+//  Copyright (c) 2013 APPZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PBClient : NSObject
+
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (PBClient *)sharedClient;
+
+@end
